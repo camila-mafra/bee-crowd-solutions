738da1fc3bf84fc1c48e454be0ce921d78e9d8f7
Camila Malaman Mafra <camila.mafra@sga.pucminas.br>
2025-12-06 16:03:28 -0300

    Altera conteúdo da main para Hello World

--- main.c
+++ main.c
@@ -3,22 +3,7 @@
 
 int main () {
 
-  char nome[10];
-  double Sal, Ven, Total;
-
-  printf("Qual seu nome?: ");
-  scanf("%s",nome);
-
-  printf("Seu nome é: %s\n", nome);
-
-  printf("Qual seu salário?: ");
-  scanf("%lf",&Sal);
-  printf("Qal o total de vendas no mes?: ");
-  scanf("%lf",&Ven);
-  
-  Total = Sal + (Ven * 0.15);
-
-  printf("Total = R$ %.2lf\n", Total);
-
+  printf("Hello World!\n");
+ 
     return 0;
 }
