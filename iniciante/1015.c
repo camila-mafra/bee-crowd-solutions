c934cfe43bfb05fff0ce9a9153f5401ebd596373
Camila Malaman Mafra <camila.mafra@sga.pucminas.br>
2025-12-17 09:02:47 -0300

    Resolve problema 1015 - Distância entre dois pontos

---
+++ iniciante/1015.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+    double x1, y1, x2, y2, distancia;
+
+    printf("Digite os valores de p1: ");
+    scanf("%lf %lf",&x1,&y1);
+
+    printf("Digite os valores de p2: ");
+    scanf("%lf %lf",&x2,&y2);
+
+    distancia = sqrt(pow(x2- x1, 2)+ pow(y2 - y1, 2));
+
+    printf("%.4lf\n", distancia);
+
+
+
+
+    return 0;
+}
