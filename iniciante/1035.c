458e4c98983526e30f33e4cd95843ba3391c9dce
Camila Malaman Mafra <camila.mafra@sga.pucminas.br>
2025-12-18 21:53:28 -0300

    Resolve problema 1035 - Teste de Seleção 1

---
+++ iniciante/1035.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int A, B, C, D;
+
+    printf("Qual valor de A?: ");
+    scanf("%d",&A);
+    printf("Qual valor de B?: ");
+    scanf("%d",&B);
+    printf("Qual valor de C?: ");
+    scanf("%d",&C);
+    printf("Qual valor de D?: ");
+    scanf("%d",&D);
+
+    if (B > C && D > A) {
+        if (C + D > A + B){ 
+
+    } if (C && D % 2 != 1) { 
+
+    }  if (A % 2 != 1) {
+        printf("Valores aceitos\n");
+
+    }
+    }   else {
+        printf("Valores nao aceitos\n");
+    }
+
+    return 0;
+}
