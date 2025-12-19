ec4f298fbfc285c45fc4e606a460161ddabaa755
Camila Malaman Mafra <camila.mafra@sga.pucminas.br>
2025-12-19 07:58:07 -0300

    Resolve problema 1036 - Fórmula Bhaskara

---
+++ iniciante/1036.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+    double a, b, c, R1, R2, delta;
+
+    printf ("Quais os valores de A, B e C?: ");
+    scanf("%lf %lf %lf",&a,&b,&c);
+
+    delta = (pow(b,2)) - (4 * a * c);
+
+    if (a == 0 || delta < 0) {
+        printf("Impossivel calcular\n");
+
+    } else {
+         R1 = ((-b) + (sqrt(delta))) / (2 * a);
+         R2 = ((-b) - (sqrt(delta))) / (2 * a);
+          
+          printf("R1 = %.5lf\n", R1);
+          printf("R2 = %.5lf\n", R2);
+    }
+
+    return 0;
+}
